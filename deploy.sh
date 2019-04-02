cf710d309e909cb2cf4d5431dfab065523fcb774
Chad Glendenin <chad@glendenin.com>
2019-04-02 18:41:31 -0400

    Use env var for deployment info

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-rsync -az public guess_who@glendenin.com:webroot/chad.glendenin.com/
+if [ x"${CCG_WEB_USER}" == x ]; then
+  echo "Set \$CCG_WEB_USER env var before running this script."
+  exit 1
+fi
+
+rsync -az public "${CCG_WEB_USER}@glendenin.com:webroot/chad.glendenin.com/"
